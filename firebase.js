6aaa9fd04b2b9a1a22e6023ce61ae88723195058
siobhangi <siobhan.goggins@gmail.com>
2021-01-30 20:06:16 +0000

    Initialize create-react-app

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA7mYTBe-K0NX8OLFXefpP6pP9btsfmt1g",
+  authDomain: "tik-tok-clone-6d5b8.firebaseapp.com",
+  projectId: "tik-tok-clone-6d5b8",
+  storageBucket: "tik-tok-clone-6d5b8.appspot.com",
+  messagingSenderId: "1000661857015",
+  appId: "1:1000661857015:web:f2f10f17e72abae033d9f2",
+  measurementId: "G-ZGXLD897TN"
+};
